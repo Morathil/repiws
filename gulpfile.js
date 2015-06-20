791fe0a597129d7fca351a9a87e353b13239010c
morathil <christoph.altmann90@gmail.com>
2015-06-20 23:07:03 +0200

    Reinit without vagrant

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require("gulp");
+var plugins = require("gulp-load-plugins")();
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var reactify = require("reactify");
+var path = require('path');
+
+var paths = {
+  scripts: ["src/js/**/*"],
+  vendor: ["src/vendor/**/*"],
+  statics: ["src/*.html", "src/img/**/*", "src/css/**/*", "src/font/**/*"]
+};
+
+gulp.task("vendor", function() {
+//  gulp.src(paths.vendor)
+//    .pipe(plugins.concat("vendor.js"))
+//    .pipe(gulp.dest("www/vendor"));
+});
+
+gulp.task("statics", function() {
+  gulp.src(paths.statics, {
+    base: "src"
+  })
+    .pipe(gulp.dest("www"));
+});
+
+gulp.task("browserify", function() {
+  var bundleStream = browserify()
+    .add("src/js/index.js")
+    .transform(reactify)
+    .bundle();
+
+  bundleStream.on("error", function(error) {
+    plugins.util.log(plugins.util.colors.red(error));
+    this.end();
+  });
+
+  bundleStream.pipe(source("index.js"))
+    .pipe(gulp.dest("www/js"));
+});
+
+gulp.task("default", ["vendor", "statics", "browserify"], function() {
+  gulp.watch(paths.scripts, ["vendor", "statics", "browserify"]);
+  gulp.watch(paths.vendor, ["vendor", "statics", "browserify"]);
+  gulp.watch(paths.statics, ["vendor", "statics", "browserify"]);
+}); 
