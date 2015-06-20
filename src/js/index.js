e3a09d14f0c77c1643b5f7038695756a1d89c578
morathil <christoph.altmann90@gmail.com>
2015-06-20 23:29:03 +0200

    Add dev npm modules

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+var React = require("react");
+var App = require("./views/App.jsx");
+
+React.render( <App /> ,
+  document.getElementById("content")
+);
---
+++ src/js/views/App.jsx
@@ -0,0 +1,23 @@
+"use strict"
+
+var React = require("react");
+
+var Repiws = React.createClass({
+  render: function() {
+    return (
+    	<div>
+		   <nav className="cyan darken-4">
+		   <div className="container">
+			    <div className="nav-wrapper">
+			      <a href="#" className="brand-logo">Repiws</a>
+			    </div>
+		    </div>
+		  </nav>    	
+      <div className={""}>
+      </div>
+    </div>
+    );
+  }
+});
+
+module.exports = Repiws;
