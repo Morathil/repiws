d00955ea51091996b3a40bc1f65e0697f96d966c
morathil <christoph.altmann90@gmail.com>
2015-06-21 19:49:00 +0200

    Clean up app.jsx

--- src/js/views/App.jsx
+++ src/js/views/App.jsx
@@ -7,17 +7,8 @@ var Repiws = React.createClass({
   render: function() {
     return (
     	<div>
-		   <nav className="cyan darken-4">
-		   <div className="container">
-			    <div className="nav-wrapper">
-			      <a href="#" className="brand-logo">Repiws</a>
-			    </div>
-		    </div>
-		  </nav>    	
-      <div className={""}>
+        <Carousel />
       </div>
-      <Carousel />
-    </div>
     );
   }
 });
