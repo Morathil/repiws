0ea5bf3dd4f82f87b1d211dbd78720c50567940a
morathil <christoph.altmann90@gmail.com>
2015-06-28 14:20:37 +0200

    Add facebook plugin (not yet uses), fix menu with max width

--- src/js/components/facebookSdk.js
+++ src/js/components/facebookSdk.js
@@ -3,16 +3,15 @@ var Parse = require("parse").Parse;
 module.exports = window.fbAsyncInit = function() {
   Parse.FacebookUtils.init({
     appId      : '499457133534807',
-    cookie     : true,  // enable cookies to allow Parse to access the session    
+    cookie     : true,  // enable cookies to allow Parse to access the session
     xfbml      : true,
     version    : 'v2.3'
   });
 };
-
 (function(d, s, id){
    var js, fjs = d.getElementsByTagName(s)[0];
    if (d.getElementById(id)) {return;}
    js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js";
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
    fjs.parentNode.insertBefore(js, fjs);
  }(document, 'script', 'facebook-jssdk'));
