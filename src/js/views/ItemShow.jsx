e3c258f03ecc1a9dc9c596097199536430e09a38
Daniel Margreiter <danielm@live.at>
2015-07-22 21:50:21 +0200

    ItemShow now loads the deep link page from ean

--- src/js/views/ItemShow.jsx
+++ src/js/views/ItemShow.jsx
@@ -1,9 +1,32 @@
+'use strict';
+
 var React = require("react");
 
 var ViewActions = require("../actions/ViewActions");
 var ENV = require("../../../.env.json");
 
 var BurgerMenu = require("./BurgerMenu.jsx");
+
+var fullscreen = {
+  width: "100%",
+  height: "100%"
+};
+
+var Iframe = React.createClass({
+
+  propTypes: {
+    src: React.PropTypes.string.isRequired,
+    onLoad: React.PropTypes.func
+  },
+
+  componentDidMount: function() {
+    this.refs.iframe.getDOMNode().addEventListener('load', this.props.onLoad);
+  },
+
+  render: function() {
+    return <iframe ref="iframe" {...this.props}/>;
+  }
+});
 
 var ItemShow = React.createClass({
   propTypes: {
@@ -12,10 +35,11 @@ var ItemShow = React.createClass({
 
   render: function() {
     return (
-      <div>
+      <div style={fullscreen}>
         <BurgerMenu style="back" />
         ITEM SHOW:
         {this.props.item.get(ENV.IMAGE_URL)}
+        <Iframe style={fullscreen} className={"deepLinkIFrame"} src={this.props.item.get("deepLink")}/>
       </div>
     );
   }
